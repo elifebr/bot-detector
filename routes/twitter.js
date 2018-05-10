62d359c4f388600c314ac30fd0c31fcb635890c7
Gabriel Morais <gabmla19@gmail.com>
2018-05-10 18:05:31 -0300

    response fix

--- routes/twitter.js
+++ routes/twitter.js
@@ -6,6 +6,7 @@ var SuspiciousUser	= require('../models/SuspiciousUser.js');
 
 var router 			= express.Router();
 var actual_key		= 0;
+var total_requests  = 0;
 var twitter			= new Twit(config.twitter_keys[actual_key]);
 
 router.get('/suspicious_users', function (req, res) {
@@ -99,14 +100,22 @@ router.get('/botcheck/:screen_name', function(req, res) {
 });
 
 router.post('/botcheck/', function(req, res) {
-	var users = req.body.screen_names;
+	var users = req.body.screen_names; // ["screen_name", "screen_name", "screen_name"]
 	var requests = 0;
 	var responses = [];
 
 	for (var i = 0; i < users.length; i++) {
+		total_requests++;
+		
+		if ((actual_key % 2 == 0 && total_requests >= 1400) || (actual_key % 2 != 0 && total_requests >= 800)) {
+			total_requests = 0;
+			actual_key += 1;
+			twitter	= new Twit(config.twitter_keys[actual_key]);
+		}
+
 		let auxUser = { user: users[i], index: i };
 
-		SuspiciousUser.findOne({screen_name: auxUser.user})
+		SuspiciousUser.findOne({screen_name: auxUser.user}) // Search if the user is already saved on the bd.
 		.catch((err) => {
 			responses[auxUser.index] = ('Mongo error');
 			requests++;
@@ -117,13 +126,13 @@ router.post('/botcheck/', function(req, res) {
 		})
 		.then((user_found) => { 
 			if (user_found) { // Suspicious user cached
-				responses[auxUser.index] = (user_found.suspicious_level);
+				responses[auxUser.index] = { screen_name: auxUser.user, value: user_found.suspicious_level.value };
 				requests++;
 
 				if (requests == users.length) {
 					done();
 				}
-			} else {
+			} else { // Suspicious_user not found
 				twitter.get('statuses/user_timeline', { screen_name: auxUser.user, count: 200, include_rts: true })
 				.catch((err) => {
 					responses[auxUser.index] = (err);
@@ -134,77 +143,10 @@ router.post('/botcheck/', function(req, res) {
 					}
 				})
 				.then((result) => {
-					if (result.resp.statusCode != 200 || !result.data[0].user) { //not_found or another error
-						responses[auxUser.index] = (result.data);
+					if (result.data.errors) { // Twitter API rate limit
+						responses[auxUser.index] = { error: result.data.errors };
 						requests++;
-
-						if (requests == users.length) {
-							done();
-						}
-					} else if (result.resp.statusCode == 429) { //rate_limit
-						
-						actual_key += 1;
-
-						if (actual_key >= 4) {
-							actual_key = 0;
-						}
-						
-						twitter	= new Twit(config.twitter_keys[actual_key]);
-		
-						twitter.get('statuses/user_timeline', { screen_name: auxUser.user, count: 200, include_rts: true })
-							.catch((err) => {
-								responses[auxUser.index] = (err);
-								requests++;
-
-								if (requests == users.length) {
-									done();
-								}
-							})
-							.then((result) => {
-								if (result.resp.statusCode != 200 || !result.data[0].user) { //not_found or another error
-									responses[auxUser.index] = (result.data);
-									requests++;
-
-									if (requests == users.length) {
-										done();
-									}
-								} else {
-									var analysis = TwitterDetector.bot_check(result.data[0].user, result.data);
-
-									if (analysis.boolean_analysis.suspicious_level.value > 0.4) { // Caching
-										var suspiciousUser = new SuspiciousUser();
-										suspiciousUser.screen_name = auxUser.user;
-										suspiciousUser.result = analysis;
-										suspiciousUser.suspicious_level = analysis.boolean_analysis.suspicious_level;
-			
-										suspiciousUser.save(function (err) {
-											if (err) {
-												responses[auxUser.index] = ('Mongo error');
-												requests++;
-
-												if (requests == users.length) {
-													done();
-												}
-											} else {
-												responses[auxUser.index] = (analysis.boolean_analysis.suspicious_level);
-												requests++;
-
-												if (requests == users.length) {
-													done();
-												}
-											}
-										});
-									} else {
-										res.status(200).json(analysis.boolean_analysis.suspicious_level);
-										requests++;
-												
-										if (requests == users.length) {
-											done();
-										}
-									}
-								}
-							});
-					} else { // 
+					} else { 
 						var analysis = TwitterDetector.bot_check(result.data[0].user, result.data);
 
 						if (analysis.boolean_analysis.suspicious_level.value > 0.4) { // Caching
@@ -222,7 +164,7 @@ router.post('/botcheck/', function(req, res) {
 										done();
 									}
 								} else {
-									responses[auxUser.index] = (analysis.boolean_analysis.suspicious_level);
+									responses[auxUser.index] = { screen_name: auxUser.user, value: analysis.boolean_analysis.suspicious_level.value };
 									requests++;
 
 									if (requests == users.length) {
@@ -231,7 +173,7 @@ router.post('/botcheck/', function(req, res) {
 								}
 							});
 						} else {
-							responses[auxUser.index] = (analysis.boolean_analysis.suspicious_level);
+							responses[auxUser.index] = { screen_name: auxUser.user, value: analysis.boolean_analysis.suspicious_level.value };
 							requests++;
 
 							if (requests == users.length) {
