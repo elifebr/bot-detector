3c35958f5c04fc08f27100d0846ba3cef387347d
Gabriel Morais <gabmla19@gmail.com>
2018-06-07 16:22:16 -0300

    index by screen_name

--- models/AnalysedUser.js
+++ models/AnalysedUser.js
@@ -2,13 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var AnalysedUserSchema = new Schema({
-  screen_name: String,
+  screen_name: {
+    type: String,
+    index: true
+  },
   created: {
       type: Date,
       default: Date.now
   },
   suspicious_level: { level: String, value: Number }
-});
+}, {autoIndex: false});
 
 var AnalysedUser = mongoose.model('AnalysedUser', AnalysedUserSchema);
 
